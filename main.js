6faf7dadeddf15a5e7918c9087aa9120dfa70240
spaceface777 <spaceface7777@gmail.com>
2018-11-30 10:01:12 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,45 @@
+const {app, BrowserWindow} = require('electron')
+
+var mainWindow;
+
+const lock = app.requestSingleInstanceLock()
+
+if (!lock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
+    }
+  })
+};
+
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({
+    width: 1024, 
+    height: 768,
+    webPreferences: {
+      nodeIntegration: false,
+//    preload: './preload.js'
+    }
+  });
+
+  mainWindow.loadURL('https://hermesmessenger.duckdns.org')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  };
+  mainWindow.maximize()
+});
+
