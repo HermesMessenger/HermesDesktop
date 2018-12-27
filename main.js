1239c02db5baddc38307ff0cb366178f3e96b440
spaceface777 <spaceface7777@gmail.com>
2018-12-27 13:11:33 +0100

    Add update checks (Fix #7)

--- main.js
+++ main.js
@@ -1,70 +1,98 @@
-const { app, BrowserWindow, ipcMain, Tray, Menu, systemPreferences } = require('electron');
-const settings = require('electron-settings');
-const isOnline = require('is-online');
-const path = require('path');
-const request = require('request');
+const { app, BrowserWindow, ipcMain, Tray, Menu, shell, systemPreferences } = require('electron')
+const settings = require('electron-settings')
+const isOnline = require('is-online')
+const path = require('path')
+const request = require('request')
+const semver = require('semver')
+
+const currentVersion = process.env.npm_package_version
+var newVersion
 
 app.setAppUserModelId('HermesMessenger.Hermes.Desktop')
 
-var reallyQuit = false;
-var mainWindow;
-var HermesURL = 'http://hermesmessenger.duckdns.org';
-// var HermesURL = 'http://hermesmessenger-testing.duckdns.org'; // Testing server
-// var HermesURL = 'http://localhost:8080'; // Uncomment this to use your local server instead of the main one (useful for testing), but remember to comment it back before pushing.
+var reallyQuit = false
+var mainWindow
+var HermesURL = 'http://hermesmessenger.duckdns.org'
+// var HermesURL = 'http://hermesmessenger-testing.duckdns.org' // Testing server
+// var HermesURL = 'http://localhost:8080' // Uncomment this to use your local server instead of the main one (useful for testing), but remember to comment it back before pushing.
 
-var icon = path.join(__dirname, 'icons/Icon.png');
+var icon = path.join(__dirname, 'icons/Icon.png')
 if (process.platform === 'darwin') {
-    icon = path.join(__dirname, 'icons/Icon_osxTemplate.png');
-};
+    icon = path.join(__dirname, 'icons/Icon_osxTemplate.png')
+}
 
 app.on('ready', () => {
 
     if (settings.get('firstRunComplete')) { // Introduction Complete
 
-        mainWindow = new BrowserWindow(MainMenu);
+        mainWindow = new BrowserWindow(MainMenu)
 
-        tray = new Tray(icon); // Background service 
-        tray.on('click', () => mainWindow.show());
-        tray.setContextMenu(Menu.buildFromTemplate(trayMenu));
-
-        mainWindow.loadFile('./web/loading.html');
-        mainWindow.maximize();
-        mainWindow.show();
+        mainWindow.loadFile('./web/loading.html')
+        mainWindow.maximize()
+        mainWindow.show()
             
         isOnline().then(online => {
             if (online) { // Device is connected to the Internet
-                request.get(HermesURL, (err, res) => { 
+                request.get(HermesURL, (err, res, body) => { 
                     if (!err && (res.statusCode == 200 || res.statusCode == 301)) { // Device can connect to Hermes, main code goes here
+                        
+                        let uuid = settings.get('uuid')
+                        let theme = systemPreferences.isDarkMode() ? 'dark' : 'light'
+                        mainWindow.loadURL(HermesURL + '/setCookie/' + uuid + '/' + theme) // API call that saves cookie on the client. It redirects to /chat if the config is valid and to /login if it isn't.
 
-                        let uuid = settings.get('uuid');
-                        let theme = systemPreferences.isDarkMode() ? 'dark' : 'light';
-                        mainWindow.loadURL(HermesURL + '/setCookie/' + uuid + '/' + theme); // API call that saves cookie on the client. It redirects to /chat if the config is valid and to /login if it isn't.
+                        tray = new Tray(icon) // Background service 
+                        tray.on('click', () => mainWindow.show())
+                        tray.setContextMenu(Menu.buildFromTemplate(trayMenu))
 
-                    } else mainWindow.loadFile('./web/error.html'); // Error connecting to Hermes server
-                }).on('error', () => mainWindow.loadFile('./web/error.html'));
 
-            } else mainWindow.loadFile('./web/noInternet.html');
+                        // Check for updates
+                        request.get({
+                            url: 'https://api.github.com/repos/HermesMessenger/HermesDesktop/releases/latest',
+                            json: true, 
+                            headers: { 'User-Agent': 'request' } 
+                        }, (err, res, body) => {
+
+                            if (!err && res.statusCode == 200) {
+                                if (body.tag_name) {
+                                    newVersion = body.tag_name
+                                    if (semver.gt(newVersion, currentVersion)) {
+                                        updateWindow = new BrowserWindow(UpdateMenu)
+                                        updateWindow.loadFile('./web/newUpdate.html')
+
+                                        updateWindow.webContents.on('will-navigate', (event, url) => { // Open links in default browser
+                                            event.preventDefault()
+                                            shell.openExternal(url)
+                                        })
+                                    }
+                                }
+                            }
+                        })
+
+                    } else mainWindow.loadFile('./web/error.html') // Error connecting to Hermes server
+                }).on('error', () => mainWindow.loadFile('./web/error.html'))
+
+            } else mainWindow.loadFile('./web/noInternet.html')
         }).catch(err => mainWindow.loadFile('./web/noInternet.html'))
 
         mainWindow.on('close', (event) => {
             if (!reallyQuit) {
-                event.preventDefault();
-                mainWindow.hide();
-                event.returnValue = false;
+                event.preventDefault()
+                mainWindow.hide()
+                event.returnValue = false
             }
-        });
+        })
 
     } else { // Introduction not complete
-        mainWindow = new BrowserWindow(TutorialMenu);
-        mainWindow.loadFile('web/tutorialPage/index.html');
+        mainWindow = new BrowserWindow(TutorialMenu)
+        mainWindow.loadFile('web/tutorialPage/index.html')
     }
-});
+})
 
 app.on('activate', () => {
     if (mainWindow === null) createWindow()
-});
+})
 
-app.on('before-quit', () => reallyQuit = true);
+app.on('before-quit', () => reallyQuit = true)
 
 /*
  __  __ _____ _   _ _   _ ____  
@@ -104,13 +132,29 @@ const TutorialMenu = {
     }
 }
 
+const UpdateMenu = {
+    width: 500, height: 300,
+    resizable: false,
+    maximizable: false, 
+    fullscreen: false, 
+    icon: icon,
+
+    title: 'Hermes Desktop',
+
+    'Content-Security-Policy': 'script-src self',
+    webPreferences: {
+        nodeIntegration: false,
+        preload: path.join(__dirname, 'preload.js')
+    }
+}
+
 const trayMenu = [
     { label: 'Hermes Messenger', enabled: false }, // Title for the context menu
     { type: 'separator'},
     { label: 'Show App', click: () => mainWindow.show() },
     { label: 'Close App', click: () => {
-        reallyQuit = true;
-        app.quit();
+        reallyQuit = true
+        app.quit()
       }
     }
 ]
@@ -124,12 +168,24 @@ const trayMenu = [
 
 */
 
+ipcMain.on('get', (event, arg) => {
+    if (arg == 'current') {
+        event.returnValue = currentVersion
+    } else if (arg == 'latest') {
+        event.returnValue = newVersion.substr(1)
+    } else event.returnValue = undefined
+})
+
+ipcMain.on('close', () => {
+    updateWindow.destroy()
+})
+
 ipcMain.on('complete', () => {
     settings.set('firstRunComplete', true)
     app.relaunch()
     app.exit()
-});
+})
 
 ipcMain.on('setUUID', (event, arg) => {
     settings.set('uuid', arg)
-});
+})
--- preload.js
+++ preload.js
@@ -1,5 +1,14 @@
 const ipc = require('electron').ipcRenderer
 
+function get (version) {
+    version = ipc.sendSync('get', version);
+    return version;
+}
+
+function close () {
+    ipc.send('close', true)
+}
+
 function sendComplete () {
     ipc.send('complete', true)
 }
@@ -8,5 +17,8 @@ function sendUUID (uuid) {
     ipc.send('setUUID', uuid)
 }
 
+window.ipc = ipc;
+window.get = get;
+window.close = close;
 window.sendComplete = sendComplete;
 window.sendUUID = sendUUID;
